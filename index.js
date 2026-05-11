2f296ade5f99af91494ea3aeb313a24b65f0ef75
BarkleyRhoat <barkleyrhoat@gmail.com>
2026-05-11 16:08:24 -0400

    got keydown event to move

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+const dodge = document.getElementById("dodger");
+dodger.style.backgroundColor = "#ff69b4";
+dodger.style.bottom = "100px";
+dodger.style.left = "180px";
+
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if(e.key === "ArrowRight"); {
+        moveDodgerRight();
+  }
+});
+
+
